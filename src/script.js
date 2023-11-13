e11bc870194af9370ae3edd1e0c4e14c11eb5269
Tatraa <tatra5342@gmail.com>
2023-11-13 17:34:16 +0100

    Final view v0.4

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 document.getElementById('resume').addEventListener('click', function () {
     // Specify the path to the PDF file
-    const filePath = 'images/example.pdf';
+    const filePath = 'images/KacperTatrockiCV.pdf';
 
     // Create a link element
     const link = document.createElement('a');
 
     // Set the download attribute and create a download URL
-    link.download = 'example.pdf';
+    link.download = 'KacperTatrockiCV.pdf';
     link.href = filePath;
 
     // Append the link to the document
